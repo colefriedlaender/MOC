99b40a7e23d19149a2c28b99d2c0800e4822c55b
colefriedlaender <colefriedlaender@outlook.de>
2021-03-15 19:32:25 +0100

    Add Stock Block to Storybook

---
+++ components/StockBlock/Stock.stories.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Story, Meta } from "@storybook/react/types-6-0";
+import Stock, { StockProps } from "./Stock";
+
+export default {
+  title: "Common/Stock",
+  component: Stock,
+} as Meta;
+
+const Template: Story<StockProps> = (args) => <Stock {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  stockName: "APPL INC.",
+  rate: "+2,45",
+  amount: "3",
+  price: "100,56",
+  total: "4000",
+};
---
+++ components/StockBlock/Stock.tsx
@@ -0,0 +1,23 @@
+import "./Stock.module.css";
+export type StockProps = {
+  stockName: string;
+  rate: string;
+  amount: number;
+  price: string;
+  total: string;
+};
+
+function Stock({ amount, price, rate, stockName, total }: StockProps) {
+  return (
+    <div className="container">
+      <div className="stockName">{stockName}</div>
+      <div className="rate">{rate}%</div>
+      <div className="amount">
+        {amount} x ${price}
+      </div>
+      <div className="total">${total}</div>
+    </div>
+  );
+}
+
+export default Stock;
