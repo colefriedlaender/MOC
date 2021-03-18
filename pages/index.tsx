cf566e6b83c095f3cf06bf1c007996b2ca3aed46
colefriedlaender <colefriedlaender@outlook.de>
2021-03-18 18:27:29 +0100

    Add Components to index.tsx + styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,32 @@
 import Head from "next/head";
+import { useEffect, useState } from "react";
 import Balance from "../components/Balance/Balance";
 import Greeting from "../components/Greeting/Greeting";
 import SettingsButtonStories from "../components/SettingsButton/SettingsButton";
+import StockListItem from "../components/StockListItem/StockListItem";
 import SubHeadline from "../components/SubHeadline/SubHeadline";
 import styles from "../styles/Home.module.css";
+import { getStocks, Stock } from "../utils/api";
+import Link from "next/link";
+
+const userName = "Cole Friedlaender";
+const date = "14.03.2021";
 
 export default function Home() {
+  const [stocks, setStocks] = useState<Stock[]>([]);
+
+  useEffect(() => {
+    getStocks().then((newStocks) => {
+      setStocks(newStocks);
+    });
+  }, []);
+  const stockItems = stocks.map((stock) => (
+    <Link href={`/stocks/${stock.id}`} key={stock.id}>
+      <a>
+        <StockListItem key={stock.id} stock={stock} />
+      </a>
+    </Link>
+  ));
   return (
     <div className={styles.page}>
       <div className={styles.container}>
@@ -14,16 +35,23 @@ export default function Home() {
           <link rel="icon" href="/Icons/material-home.svg.svg" />
         </Head>
         <header className={styles.header}>
-          <Greeting name={"Max Mustermann"} />
-          <SettingsButtonStories
-            onClick={() => {
-              alert("You pressed the Settings Button");
-            }}
-          />
-          <Balance total={10000} returnValue={2.45} />
+          <Greeting name={userName} />
+          <section className={styles.settingsButton}>
+            <SettingsButtonStories
+              onClick={() => {
+                alert("You pressed the Settings Button");
+              }}
+            />
+          </section>
+          <section className={styles.balance}>
+            <Balance total={10000} returnValue={2.45} />
+          </section>
         </header>
         <main className={styles.main}>
-          <SubHeadline date={"14.03.2002"} />
+          <SubHeadline date={date} />
+          <section>
+            <div className={styles.list}>{stockItems}</div>
+          </section>
         </main>
         <footer className={styles.footer}></footer>
       </div>
