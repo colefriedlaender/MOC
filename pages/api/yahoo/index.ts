e7976b3dc81491578d5a1a6cb25a0bf2a74558d1
colefriedlaender <colefriedlaender@outlook.de>
2021-03-22 22:14:45 +0100

    Add Samples and api/yahoo index.ts

---
+++ pages/api/yahoo/index.ts
@@ -0,0 +1,19 @@
+export async function getStockInformation() {
+  const response = await fetch(
+    "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-summary?symbol=AAPL&region=DE",
+    {
+      method: "GET",
+      headers: {
+        "x-rapidapi-key": "3fab7dc221msh592492bed2b6837p1b7a17jsnb4f5b866bad9",
+        "x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com",
+      },
+    }
+  )
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+  return response;
+}
