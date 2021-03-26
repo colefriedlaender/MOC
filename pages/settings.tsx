7ab6b06a9b493b3ef84acd6e031c789dc5e542ea
colefriedlaender <colefriedlaender@outlook.de>
2021-03-26 16:16:05 +0100

    Chnaged Settings.tsx

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -15,11 +15,12 @@ export default function Settings() {
       preserveAspectRatio: "xMidYMid slice",
     },
   };
+
   return (
     <div className={styles.container}>
       <Head>
         <title>MOC</title>
-        <link rel="icon" href="/Icons/favicon.png" />
+        <link rel="icon" href="/Icons/favicon.toilett.png" />
       </Head>
       <header className={styles.header}>
         <BackButton
@@ -34,15 +35,6 @@ export default function Settings() {
       <main className={styles.main}>
         <div className={styles.loading}>
           <Lottie options={defaultOptions} height={220} width={300} />
-        </div>
-        <div>
-          <button
-            onClick={() => {
-              getStockInformation();
-            }}
-          >
-            API
-          </button>
         </div>
       </main>
       <footer className={styles.footer}></footer>
