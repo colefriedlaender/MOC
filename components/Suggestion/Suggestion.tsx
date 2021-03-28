79fb8c122d8673c50f4ac23fc6f403b0836f76e5
colefriedlaender <colefriedlaender@outlook.de>
2021-03-28 16:03:31 +0200

    Added Suggestion Component

---
+++ components/Suggestion/Suggestion.tsx
@@ -0,0 +1,15 @@
+import styles from "../Suggestion/Suggestion.module.css";
+export type SuggestionProps = {
+  name: string;
+  price: number;
+};
+function Suggestion({ name, price }: SuggestionProps) {
+  return (
+    <div className={styles.container}>
+      <div className={styles.name}>{name}</div>
+      <div className={styles.price}>${price}</div>
+    </div>
+  );
+}
+
+export default Suggestion;
