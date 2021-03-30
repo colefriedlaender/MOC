e972aa95a3ce567f75181cb5fa490cbada8e5977
colefriedlaender <colefriedlaender@outlook.de>
2021-03-30 16:27:18 +0200

    Added Headline Component

---
+++ components/SubHeadline/Headline.tsx
@@ -0,0 +1,10 @@
+import styles from "../SubHeadline/Headline.module.css";
+
+function Headline() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.subHeader}>Suggestions</div>
+    </div>
+  );
+}
+export default Headline;
