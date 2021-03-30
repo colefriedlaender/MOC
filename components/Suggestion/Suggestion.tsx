ea23486d48ac9187ef6a293bb71d8e5722f1e34e
colefriedlaender <colefriedlaender@outlook.de>
2021-03-30 16:27:43 +0200

    Updated Suggestion Component

--- components/Suggestion/Suggestion.tsx
+++ components/Suggestion/Suggestion.tsx
@@ -4,10 +4,11 @@ export type SuggestionProps = {
   price: number;
 };
 function Suggestion({ name, price }: SuggestionProps) {
+  const sum = price.toFixed(2);
   return (
     <div className={styles.container}>
       <div className={styles.name}>{name}</div>
-      <div className={styles.price}>${price}</div>
+      <div className={styles.price}>${sum}</div>
     </div>
   );
 }
