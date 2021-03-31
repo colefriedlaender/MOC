8e4d1fb31e91a823fb69891f4a2cffda2f98cc14
colefriedlaender <colefriedlaender@outlook.de>
2021-03-31 11:33:14 +0200

    Changed Navbar Component

--- components/Navbar/Nabar.tsx
+++ components/Navbar/Nabar.tsx
@@ -1,6 +1,38 @@
+import Link from "next/link";
+import { MouseEventHandler } from "react";
 import styles from "../Navbar/Navbar.module.css";
+export type NavbarProps = {
+  onClick: MouseEventHandler<HTMLImageElement>;
+};
 
-function Navbar() {
-  return <div className={styles.container}></div>;
+function Navbar({ onClick }: NavbarProps) {
+  return (
+    <div className={styles.container}>
+      <Link href={`/searchPage`}>
+        <a>
+          <img className={styles.icon} src="/Icons/add.svg" alt="Add Button" />
+        </a>
+      </Link>
+      <Link href={`/`}>
+        <a>
+          <img
+            className={styles.icon}
+            src="/Icons/home.svg"
+            alt="Home Button"
+          />
+        </a>
+      </Link>
+      <img
+        className={styles.icon}
+        src="/Icons/community.svg"
+        alt="Community Button"
+        onClick={onClick}
+      />
+    </div>
+  );
 }
 export default Navbar;
+
+export function onClick() {
+  return alert("This function will come soon");
+}
