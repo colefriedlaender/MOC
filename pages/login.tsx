3d19672a26bfe6ce789201fb15cb2a49f88ab237
colefriedlaender <colefriedlaender@outlook.de>
2021-04-06 18:46:25 +0200

    Add Login Page

---
+++ pages/login.tsx
@@ -0,0 +1,45 @@
+import { useRouter } from "next/dist/client/router";
+import Head from "next/head";
+import { useState } from "react";
+import LoginEmail from "../components/LoginEmail/LoginEmail";
+import LoginPassword from "../components/LoginPassword/LoginPassword";
+import SubmittButton from "../components/SubmitButton/SubmitButton";
+import styles from "../styles/login.module.css";
+export default function Login() {
+  const [email, setEmail] = useState(null);
+  const [password, setPassword] = useState(null);
+  const router = useRouter();
+
+  const onChangeEmail = (e) => {
+    setEmail(e.target.value);
+  };
+  const onChangePassword = (e) => {
+    setPassword(e.target.value);
+  };
+  console.log(password);
+  console.log(email);
+  const handleSubmit = () => {
+    // if (email == "colefriedlaender@outlook.de" && password == 1234) {
+    router.push("/portfolio");
+    // }
+    // return alert("The password is wrong");
+  };
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>MOC</title>
+        <link rel="icon" href="/Icons/favicon.money.png" />
+      </Head>
+      <header className={styles.header}>
+        <img src="/Icons/logoMOC.svg" alt="Logo" />
+      </header>
+      <main className={styles.main}>
+        <LoginEmail name={"Email Adress"} onChange={onChangeEmail} />
+        <LoginPassword name={"Password"} onChange={onChangePassword} />
+        <SubmittButton onClick={handleSubmit} content={"LOGIN"} />
+      </main>
+      <footer className={styles.footer}></footer>
+    </div>
+  );
+}
