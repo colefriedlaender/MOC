e1549e839fcc4a1efbdf4a653cc3cc71dbfee1c5
colefriedlaender <colefriedlaender@outlook.de>
2021-04-08 21:43:55 +0200

    Updated Settings Page

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -2,16 +2,29 @@ import Head from "next/head";
 import BackButton from "../components/BackButton/BackButton";
 import Topic from "../components/Topic/Topic";
 import styles from "../styles/settings.module.css";
-import Lottie from "react-lottie";
-import animationData from "../lotties/construction.json";
+
+import LoginPassword from "../components/LoginPassword/LoginPassword";
+import SubmittButton from "../components/SubmitButton/SubmitButton";
+import { useState } from "react";
+import { currentPassword } from "./login";
+import About from "../components/AboutMe/About";
 export default function Settings() {
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: animationData,
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice",
-    },
+  const [oldPass, setOldPass] = useState(null);
+  const [newPass, setNewPass] = useState(null);
+  const onChangeOldPass = (e) => {
+    setOldPass(e.target.value);
+  };
+  const onChangeNewPass = (e) => {
+    setNewPass(e.target.value);
+  };
+  const handleSubmit = () => {
+    if (oldPass == currentPassword) {
+      alert(`You changed your PassWord to ${newPass}`);
+    } else {
+      alert(`Your password is wrong`);
+      setOldPass("");
+      setNewPass("");
+    }
   };
 
   return (
@@ -31,11 +44,27 @@ export default function Settings() {
         </section>
       </header>
       <main className={styles.main}>
-        <div className={styles.loading}>
-          <Lottie options={defaultOptions} height={300} width={300} />
+        <div className={styles.login}>
+          <LoginPassword
+            name={"Old Password"}
+            onChange={onChangeOldPass}
+            value={oldPass}
+          />
+        </div>
+        <div className={styles.login}>
+          <LoginPassword
+            name={"New Password"}
+            onChange={onChangeNewPass}
+            value={newPass}
+          />
+        </div>
+        <div className={styles.login}>
+          <SubmittButton onClick={handleSubmit} content={"Change Password"} />
+        </div>
+        <div className={styles.login}>
+          <About />
         </div>
       </main>
-      <footer className={styles.footer}></footer>
     </div>
   );
 }
